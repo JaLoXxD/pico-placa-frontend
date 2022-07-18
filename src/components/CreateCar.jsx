43749781cad4a109fa99f5f29d9e4a85333c25ab
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-07-18 18:17:45 -0500

    [ADD] CreateCar form

--- src/components/CreateCar.jsx
+++ src/components/CreateCar.jsx
@@ -1,5 +1,19 @@
 export const CreateCar = () => {
-  return (
-    <div>CreateCar</div>
-  )
-}
+	return (
+		<>
+			<div className="formContainer">
+				<form>
+					<div className="customInput">
+						<label htmlFor="placa">Placa:</label>
+						<input className="test" id="placa" type="text" />
+					</div>
+					<div className="customInput">
+						<label htmlFor="fecha">Fecha y Hora:</label>
+						<input className="test" id="fecha" type="datetime-local" />
+					</div>
+					<input type="submit" value="Search" className="btn btn-success" />
+				</form>
+			</div>
+		</>
+	);
+};
