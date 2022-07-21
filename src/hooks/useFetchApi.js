5f7f74ae8cf58e26a165cadbee6c46a982d6b405
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-07-20 21:51:01 -0500

    [ADD] tests on useFetchApi hook

--- src/hooks/useFetchApi.js
+++ src/hooks/useFetchApi.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { checkCirculation, addCar, getAllCars } from "../helpers/fetchApi";
 
 export const useFetchApi = () => {
---
+++ tests/components/hooks/useFetchApi.test.js
@@ -0,0 +1,19 @@
+import { useFetchApi } from "../../../src/hooks/useFetchApi";
+import { renderHook, waitFor } from "@testing-library/react";
+
+describe("tests on useFetchApi hook", () => {
+	const formData = {
+		placa: "PUH-344",
+		date: "2022-07-20T21:25:20",
+	};
+
+	test("should return the initial state", async () => {
+		const { result } = renderHook(() => useFetchApi());
+		const { checkCarPlate, allCars, isLoading, newCar } = result.current;
+
+		expect(isLoading).toBeTruthy();
+		expect(allCars).toBeTruthy();
+		expect(checkCarPlate).toBeTruthy();
+		expect(newCar).toBeTruthy();
+	});
+});
